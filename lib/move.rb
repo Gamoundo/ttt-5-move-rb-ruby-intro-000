dbeba1dd72baed7e2bb8c68192c8a9360eff2e8b
Akil Griffiths <akilgg@gmail.com>
2020-04-10 21:26:37 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,8 +13,8 @@ def input_to_index(user_input)
   converted_input = user_input.to_i - 1
 end
 
-def move(board, integer, " x ")
+def move(board, integer, ' "x" ')
 
-  board[input_to_index(user_input)] =  " x "
+  board[input_to_index(user_input)] =  ' "x" '
 
 end
